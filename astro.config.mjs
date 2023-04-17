de2f8743417f53644d064ce735391c0c501bf78d
Megumi Ando <remiria@flame-daybreak.net>
2023-04-17 23:55:10 +0900

    site を入れる

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Vue to support Vue components.
-	site: 'https://fereria.github.io',
-	base: '/astro_docs',	
-	outDir: "./docs",
-	integrations: [vue()],
+  site: '',
+  base: '/astro_docs',
+  outDir: './docs',
+  integrations: [
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react(),
+   vue()],
 });
